1ac34f9d5eb2321cafc889ffaa62b33350e5313a
CPerez816 <Carlosocbjj@gmail.com>
2020-08-02 22:33:33 -0400

    installed axios and dotenv, created api logic

--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,6 +1,18 @@
-const api = {
-  getUser(username) {
+//Requiring Axios & Dotenv in lines 2&3
+const axios = require("axios");
+require("dotenv").config();
 
+//Generates Github Username
+const api = {
+  getUsername(username) {
+    return axios.get(
+      `https://api.github.com/users/${username}?client_id=${
+        process.env.CLIENT_ID
+        }&client_secret=${process.env.CLIENT_SECRET}`
+    )
+    .catch(error => {
+      console.log(`You Suck, Can't find  Username `);
+    });
   }
 };
 
